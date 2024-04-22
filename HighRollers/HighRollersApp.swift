f873918c2620c5ce8e5db149d0db8e727661912e
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-04-22 22:28:16 +0200

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by charlene hoareau on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
